14b389fe0f617d0b15cffa38e992f6a6a35130ac
hoofar <hup128@psu.edu>
2014-09-25 12:54:46 -0400

    setting added top link

--- src/com/app/pictolike/SettingsScreenActivity.java
+++ src/com/app/pictolike/SettingsScreenActivity.java
@@ -1,24 +1,58 @@
 package com.app.pictolike;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
-	public class SettingsScreenActivity extends Fragment{
-//		@Override
-//		public void onCreate(Bundle savedInstanceState) {
-//	        super.onCreate(savedInstanceState);
-//	        addPreferencesFromResource(R.layout.activity_settingsscreen);
-//	    }
+	public class SettingsScreenActivity extends Activity implements OnItemClickListener{
+		ListView listView;
+		WebView webView;
 		
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.activity_settingsscreen, container,
-					false);
-			return rootView;
-		}
+	     
+	    /** Called when the activity is first created. */
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_settingsscreen);
+	         
+	        listView = (ListView) findViewById(R.id.listSettings);
+	        listView.setOnItemClickListener(this);        
+	    }
+	 
+	    /*
+	     * Parameters:
+	        adapter - The AdapterView where the click happened.
+	        view - The view within the AdapterView that was clicked
+	        position - The position of the view in the adapter.
+	        id - The row id of the item that was clicked.
+	     */
+	    @Override
+	    public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+	    	String name = String.valueOf(((TextView) view).getText());
+	    	Log.i("divy",name);
+	    	
+	    if( name.equals("Terms of Use")){
+	    		String url = "http://itshoofar.com";
+		    	Intent i = new Intent(Intent.ACTION_VIEW);
+		    	i.setData(Uri.parse(url));
+		    	startActivity(i);
+		    		Log.i("divy","i run");
+	    	}
+	    	
+	    	
+	    }
+	    
+	  
 	}
