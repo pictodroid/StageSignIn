96432f97ea2eaaadaf36e7c955bc9c6779014e67
hoofar <hup128@psu.edu>
2014-09-30 19:37:03 -0400

    dob updated

--- src/com/app/pictolike/SignUpActivity.java
+++ src/com/app/pictolike/SignUpActivity.java
@@ -1,7 +1,11 @@
 package com.app.pictolike;
 
+import java.util.Calendar;
+
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -15,6 +19,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -25,15 +30,19 @@ import com.app.pictolike.mysql.MySQLCommand;
 import com.app.pictolike.mysql.MySQLConnect;
 
 public class SignUpActivity extends Activity  {
-
-
+    
+	
+	private Calendar cal;
+	private int day;
+	private int month;
+	private int year;
 
 	EditText m_edtUserName;
 	EditText m_edtEmail;
 	EditText m_edtPassword;
 	EditText m_edtBirthday;
 	
-	String sel_gen;
+	String sel_gen= "" ;
  
 	
 	private MySQLCommand.OnCompleteListener m_oSqlListener = new MySQLCommand.OnCompleteListener() {
@@ -77,16 +86,21 @@ public class SignUpActivity extends Activity  {
 		 m_edtPassword = (EditText) findViewById(R.id.reg_password_edittext);
 		 m_edtBirthday= (EditText) findViewById(R.id. reg_birthday_edittext);
 
+		 	cal = Calendar.getInstance();
+			day = cal.get(Calendar.DAY_OF_MONTH);
+			month = cal.get(Calendar.MONTH);
+			year = cal.get(Calendar.YEAR);
 		
-		findViewById(R.id.sign_up_btn).setOnClickListener(
-				new OnClickListener() {
+		findViewById(R.id. reg_birthday_edittext).setOnClickListener(
+		new OnClickListener() {
 
-					@Override
-					public void onClick(View v) {
-						onSignUp();
-					}
-				});
-		
+			@Override
+			public void onClick(View v) {
+				//onSignUp();
+				showDialog(0);
+				Log.d("Birthday", "Inside Birthday Button");
+			}
+		});
 		
 		findViewById(R.id.reg_email_edittext).setOnClickListener(
 				new OnClickListener() {
@@ -137,7 +151,9 @@ public class SignUpActivity extends Activity  {
 
 					@Override
 					public void onClick(View v) {
+						if(validate()){
 						onSignUp();
+						}
 						Log.d("Login Button", "select status");
 					}
 				});
@@ -145,7 +161,30 @@ public class SignUpActivity extends Activity  {
 	 
 	}
 
-
+	public boolean validate(){
+		if(m_edtEmail.getText().toString().isEmpty()){
+			Toast.makeText(SignUpActivity.this, "Please Fill Email", Toast.LENGTH_SHORT).show();
+			return false;
+		}
+		else if(m_edtPassword.getText().toString().isEmpty()){
+			Toast.makeText(SignUpActivity.this, "Please Fill Password", Toast.LENGTH_SHORT).show();
+			return false;
+		}
+		else if(m_edtBirthday.getText().toString().isEmpty()){
+			Toast.makeText(SignUpActivity.this, "Please Select Birthday", Toast.LENGTH_SHORT).show();
+			return false;
+		}
+		else if(sel_gen.isEmpty()){
+			Toast.makeText(SignUpActivity.this, "Please Select Gender", Toast.LENGTH_SHORT).show();
+			return false;
+		}
+		else {
+			return true;
+		}
+	
+	}
+	
+	
 	public void onSignUp() {
 		
 		String email   = m_edtEmail.getText().toString();
@@ -168,4 +207,18 @@ public class SignUpActivity extends Activity  {
 				Secure.ANDROID_ID);
 		return deviceId;
 	}
+	
+
+	@Override
+	@Deprecated
+	protected Dialog onCreateDialog(int id) {
+		return new DatePickerDialog(this, datePickerListener, year, month, day);
+	}
+	private DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
+		public void onDateSet(DatePicker view, int selectedYear,
+				int selectedMonth, int selectedDay) {
+			m_edtBirthday.setText(selectedDay + " / " + (selectedMonth + 1) + " / "
+					+ selectedYear);
+		}
+	};
 }
